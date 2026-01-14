acfca123d58aaa1b28a3cf0b3d6ad461e8e920f8
vedsen1 <vedsen29@gmail.com>
2026-01-14 11:31:33 +0530

    double navbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,14 +5,14 @@ import { Story } from '../components/sections/Story';
 import { ExperienceReviews } from '../components/sections/ExperienceReviews';
 import { WhyRobusta } from '../components/sections/WhyRobusta';
 import { RobustaSeeds } from '../components/sections/RobustaSeeds';
-import { Navbar } from '@/components/layout/Navbar';
+
 import { Footer } from '@/components/sections/Footer';
 
 export default function Home() {
   return (
     <>
       <main className="relative z-10">
-        <Navbar/>
+        
         <HeroSlideshow />
         <Hero />
         <Story />
